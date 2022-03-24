89c179cce882d225eeeb583fe3ac791272937aa4
Manny <mannysth@yahoo.com>
2022-03-24 16:08:15 -0700

    creates server and pushes to github

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// write your code here
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log('Received ' + req.method + ' request for' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World');
+});
+
+server.listen(8080, '127.0.0.1', null, () => {
+    console.log('Server is listening on localhost:8080');
+});
